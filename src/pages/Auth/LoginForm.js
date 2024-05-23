4df59ae50ae693378390b0cea294575c84f7a677
L01C4 <boy-unfind@proton.me>
2024-05-23 23:29:40 +0300

    fix: profile page

--- src/pages/Auth/LoginForm.js
+++ src/pages/Auth/LoginForm.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Button, Alert, Container } from 'react-bootstrap';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Form, Button, Alert, Container } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [formData, setFormData] = useState({
-    nom: '',
-    password: ''
+    nom: "",
+    password: "",
   });
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -20,30 +20,28 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    axios.post(`${process.env.REACT_APP_API_URL}/users/login`, formData)
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/users/login`, formData)
       .then((r) => {
-        localStorage.setItem('token', r.data.token);
-        localStorage.setItem('user', JSON.stringify(r.data.user));
-        console.log(localStorage.getItem('user'));
-        setSuccess('Connexion réussie!');
+        localStorage.setItem("token", JSON.stringify(r.data.token));
+        localStorage.setItem("user", JSON.stringify(r.data.user));
+        setSuccess("Connexion réussie!");
         setIsAuthenticated(true);
+        navigate("/");
       })
       .catch((e) => console.log(e));
-
-    navigate("/");
   };
-
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       setIsAuthenticated(true);
       console.log(isAuthenticated);
-      setSuccess('Vous êtes déjà connecté.');
+      setSuccess("Vous êtes déjà connecté.");
     }
   }, [isAuthenticated]);
 
   return (
-    <Container style={{ maxWidth: '400px', marginTop: '2rem' }}>
+    <Container style={{ maxWidth: "400px", marginTop: "2rem" }}>
       <h2 className="text-center">Se connecter</h2>
       {error && <Alert variant="danger">{error}</Alert>}
       {success && <Alert variant="success">{success}</Alert>}
--- src/pages/Dashboard/ProfilePage.js
+++ src/pages/Dashboard/ProfilePage.js
@@ -1,52 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const ProfilePage = () => {
   const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
-  const [token, setToken] = useState(null);
 
-  // useEffect(() => {
-  //   const fetchUserData = async () => {
-  //     try {
-  //       console.log(user._id);
-  //       const response = await axios.get(`${process.env.REACT_APP_API_URL}/users/${user._id}`, {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`
-  //         }
-  //       });
-  //       setUserData(response.data);
-  //     } catch (error) {
-  //       console.log(error);
-  //       setError('Une erreur s\'est produite lors de la récupération des données utilisateur.');
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   );
+  const user_l = localStorage.getItem("user");
+
   useEffect(() => {
-    const token_l = localStorage.getItem('token');
-    const user_l  = localStorage.getItem("user");
-    const user = JSON.parse(user_l);
-    setUserData(user);
-    setToken(token_l);
-  }, []);
-
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // if (error) {
-  //   return <div>{error}</div>;
-  // }
+    setUserData(JSON.parse(user_l));
+  }, [user_l]);
 
   return (
-    <div className="container" style={{ maxWidth: '600px', marginTop: '2rem' }}>
+    <div className="container" style={{ maxWidth: "600px", marginTop: "2rem" }}>
       <h2>Profil de l'utilisateur</h2>
-      <p><strong>Nom:</strong> {userData.nom}</p>
-      <p><strong>Prénom:</strong> {userData.prenom}</p>
-      <p><strong>Grade:</strong> {userData.grade}</p>
-      <p><strong>Unité:</strong> {userData.unite}</p>
+      <p>
+        <strong>Nom:</strong> {userData?.nom}
+      </p>
+      <p>
+        <strong>Prénom:</strong> {userData?.prenom}
+      </p>
+      <p>
+        <strong>Grade:</strong> {userData?.grade}
+      </p>
+      <p>
+        <strong>Unité:</strong> {userData?.unite}
+      </p>
     </div>
   );
 };
